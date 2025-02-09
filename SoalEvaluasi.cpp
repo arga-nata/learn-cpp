b5ddaed4cd45be7151bc1424680eb581eff2e7b2
Arganata <arganata.on@gmail.com>
2025-02-09 13:59:32 +0700

    Create SoalEvaluasi.cpp

---
+++ SoalEvaluasi.cpp
@@ -0,0 +1,20 @@
+/*Tulis program yang meminta pengguna untuk memasukkan sebuah kata dan kemudian menentukan apakah kata tersebut palindrom
+atau tidak.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string kata, simpanKata;
+
+    cout << "Masukkan sebuah kata: ";
+    getline(cin, kata);
+
+    kata == simpanKata;
+
+    cout << kata;
+
+    return 0;
+}
